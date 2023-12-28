be411858f374b0db2265323a8bbc491aafabfef0
shion0224 <shion8214@gmail.com>
2023-12-28 15:34:28 +0900

    adminのAPI

--- public/admin.js
+++ public/admin.js
@@ -1,19 +1,19 @@
 window.onload = async () => {
-  // const current_mode = await fetch("/getmode");
+  const current_mode = await fetch("/getmode");
   // if (current_mode == 0) {
   // }
-  document.getElementById("input").disabled = true;
+  // document.getElementById("input").disabled = true;
 };
-document.getElementById("input").onclick = function () {
-  // const response = await fetch("/setmode?mode=0");
-  document.getElementById("input").disabled = true;
-  document.getElementById("omikuji").disabled = false;
+document.getElementById("input").onclick = async () => {
+  await fetch("/setmode?mode=0");
+  // document.getElementById("input").disabled = true;
+  // document.getElementById("omikuji").disabled = false;
 };
-document.getElementById("omikuji").onclick = function () {
-  // const response = await fetch("/setmode?mode=1");
-  document.getElementById("input").disabled = false;
-  document.getElementById("omikuji").disabled = true;
+document.getElementById("omikuji").onclick = async () =>{
+  await fetch("/setmode?mode=1");
+  // document.getElementById("input").disabled = false;
+  // document.getElementById("omikuji").disabled = true;
 };
-document.getElementById("delete").onclick = function () {
-  // const response = await fetch("/clear");
+document.getElementById("delete").onclick = async () =>{
+  await fetch("/clear");
 };
