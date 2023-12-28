846e6223b150626c0902076ce680cd67e2c27e76
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-12-28 14:04:13 +0900

    fix header and space input

--- public/input.js
+++ public/input.js
@@ -1,12 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sendButton = document.getElementById('send');
-    const message = document.getElementById('message');
-    let count = 0;
-    console.log('DOMContentLoaded');
+document.addEventListener("DOMContentLoaded", () => {
+  const sendButton = document.getElementById("send");
+  const message = document.getElementById("message");
+  let count = 0;
+  console.log("DOMContentLoaded");
 
-    sendButton.addEventListener('click', () => {
-        count++;
-        message.textContent = `送信しました！${count}件目`;
-        document.getElementById('form').value = '';
-    });
+  sendButton.addEventListener("click", () => {
+    if (document.getElementById("form").value == "") {
+      message.textContent = `空欄では送信できません`;
+    } else {
+      count++;
+      message.textContent = `送信しました！${count}件目`;
+      document.getElementById("form").value = "";
+    }
+  });
 });
