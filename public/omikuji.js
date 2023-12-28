259447236e5318a10836e8896ee40eac11df259f
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-12-28 14:29:12 +0900

    fix page switching

--- public/omikuji.js
+++ public/omikuji.js
@@ -1,4 +1,8 @@
 window.onload = async function () {
+  let mode = 1;
+  if (!mode) {
+    window.location.replace("input.html");
+  }
   // let text = document.getElementById("text");
   // const json = await fetch("/getFT");
   // let target = Math.random();
