0fbce8592a5ce12d7e746ed776b2f454e7aa12dc
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-12-28 14:09:51 +0900

    add jump button

--- public/omikuji.js
+++ public/omikuji.js
@@ -1,5 +1,9 @@
 window.onload = async function () {
-  let text = document.getElementById("text");
-  const json = await fetch("/getFT");
-  let target = Math.random();
+  // let text = document.getElementById("text");
+  // const json = await fetch("/getFT");
+  // let target = Math.random();
+};
+
+document.getElementById("jump").onclick = function () {
+  window.location.href = "review.html";
 };
