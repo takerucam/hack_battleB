a195e41b240adae278523dc3345d37b072a48f6e
shion0224 <shion8214@gmail.com>
2023-12-28 15:18:48 +0900

    inputのAPIのところ

--- public/input.js
+++ public/input.js
@@ -8,13 +8,15 @@ window.onload = function () {
   }
 };
 
-document.getElementById("send").onclick = function () {
+document.getElementById("send").onclick = async () =>  {
   const message = document.getElementById("message");
+  const form = document.getElementById("form");
   if (document.getElementById("form").value == "") {
     message.textContent = `空欄では送信できません`;
   } else {
     count++;
     message.textContent = `送信しました！${count}件目`;
+    const response = await fetch("https://hackbattle-b.onrender.com/sendTitle?title=" + form.value);
     document.getElementById("form").value = "";
   }
 };
